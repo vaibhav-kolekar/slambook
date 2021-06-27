599eb97336f90a39b2656d7d70925493cf2a98ad
Vaibhav Kolekar <68164031+vaibhav-kolekar@users.noreply.github.com>
2021-06-27 22:04:00 +0530

    Update settings.py

--- slam/slam/settings.py
+++ slam/slam/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'rssi5u$d5qk4v^pz&s%lco4q4%!qlotjd51tu-vwzxjgew0icj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sai-slambook.herokuapp.com',
+                '127.0.0.1']
 
 
 # Application definition
